56b100cbdb866a9e15567c4e25128fd91a58987e
p37mta <p37mta@gmail.com>
2023-03-04 20:23:25 +0700

    add multiple changes

--- contracts/core/PermissiveAccount.sol
+++ contracts/core/PermissiveAccount.sol
@@ -7,15 +7,31 @@ import "../interfaces/IPermissiveAccount.sol";
 import "@account-abstraction/contracts/interfaces/IEntryPoint.sol";
 import "../interfaces/Permission.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract PermissiveAccount is BaseAccount, IPermissiveAccount, Ownable {
-    mapping(bytes32 => uint256) feeUsedForPermission;
-    mapping(bytes32 => uint256) valueUsedForPermission;
+    mapping(bytes32 => uint256) private _feeUsedForPermission;
+    mapping(bytes32 => uint256) private _valueUsedForPermission;
+    mapping(address => bytes32) private _operatorPermission;
+
+    uint96 private _nonce;
+    bytes32 public merkleRoot;
 
     function setOperatorPermissions(
         address operator,
-        bytes32 merkleRootPermissions
-    ) external onlyOwner {}
+        bytes32 merkleRootPermissions,
+        bytes32[] calldata merkleRootProof
+    ) external onlyOwner {
+        require(
+            MerkleProof.verify(
+                merkleRootProof,
+                merkleRoot,
+                merkleRootPermissions
+            ),
+            "Incorrect Proof"
+        );
+        _operatorPermission[operator] = merkleRootPermissions;
+    }
 
     function isGrantedOperator(address operator) external view returns (bool) {
         return false;
@@ -25,17 +41,20 @@ contract PermissiveAccount is BaseAccount, IPermissiveAccount, Ownable {
         address operator,
         bytes32 merkleRootPermissions
     ) external view returns (bool) {
-        return false;
+        bytes32 storedRoot = _operatorPermission[operator];
+        return storedRoot == merkleRootPermissions;
     }
 
-    function isValidPermission(
-        Permission memory permission
-    ) external pure returns (bool) {
+    function isValidPermission(Permission memory permission)
+        external
+        pure
+        returns (bool)
+    {
         return false;
     }
 
     function nonce() public view override returns (uint256) {
-        return 0;
+        return _nonce;
     }
 
     function entryPoint() public view override returns (IEntryPoint) {
@@ -73,9 +92,12 @@ contract PermissiveAccount is BaseAccount, IPermissiveAccount, Ownable {
         return 2;
     }
 
-    function _validateAndUpdateNonce(
-        UserOperation calldata userOp
-    ) internal override {}
+    function _validateAndUpdateNonce(UserOperation calldata userOp)
+        internal
+        override
+    {
+        require(_nonce++ == userOp.nonce, "account: invalid nonce");
+    }
 
     function _payPrefund(uint256 missingAccountFunds) internal override {
         if (missingAccountFunds != 0) {
